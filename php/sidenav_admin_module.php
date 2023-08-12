581e257fb88b3149e229b936a79be72e4caeb5cb
RicardoRivadeneira <87491407+RicardoRivadeneira@users.noreply.github.com>
2023-08-11 20:17:56 -0600

    Actualizo sidenav_admin_module.php

--- php/sidenav_admin_module.php
+++ php/sidenav_admin_module.php
@@ -1,8 +1,8 @@
 <div id="mySidenav" class="sidenav">
   <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-  <a href="#" class="sidenav_inicio">Inicio</a>
-  <a href="#" class="sidenav_agregar_usuarios">Agregar usuarios</a>
-  <a href="#" class="sidenav_ver_usuarios">Ver usuarios</a>
+  <a href="./administrador.php" class="sidenav_inicio">Inicio</a>
+  <a href="./agregarUsuario.php" class="sidenav_agregar_usuarios">Agregar usuarios</a>
+  <a href="./verUsuarios.php" class="sidenav_ver_usuarios">Ver usuarios</a>
   <a href="#" class="sidenav_ver_actividad_usuarios">Ver actividad de usuarios</a>
   <a href="#" class="sidenav_ver_reportes">Ver reportes</a>
   <a href="#" class="sidenav_editar_compra">Editar compra</a>
